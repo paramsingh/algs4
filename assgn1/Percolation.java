6bd29720aaabace3ad7e4f4f7598bebba593fc21
Param Singh <paramsingh258@gmail.com>
2015-06-28 00:48:05 +0530

    Solve backwash by using two QF data structures.
    One with bottom and one without. 100%

--- assgn1/Percolation.java
+++ assgn1/Percolation.java
@@ -3,10 +3,12 @@
  * Date: 25/06/2015
  */
 public class Percolation {
-    // boolean grid for all sites
+
     private boolean[][] id;
 
     private WeightedQuickUnionUF qf;
+    private WeightedQuickUnionUF nf;
+
     private int size;
     private int top;
     private int bottom;
@@ -16,24 +18,24 @@ public class Percolation {
         if (N <= 0) {
             throw new IllegalArgumentException("N should be greater than 0");
         }
-
-        id = new boolean[N][N];
-
         // quickfind structure for storing connections
         // contains n*n + 2 values because I am using
         // 2 extra spots to see if connected to the top
         // and bottom
         qf = new WeightedQuickUnionUF(N*N + 2);
 
+        nf = new WeightedQuickUnionUF(N*N + 1);
+
+        id = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
+                id[i][j] = false;
+        }
+
         size = N;
         top = size * size;
         bottom = size * size + 1;
 
-        int i, j;
-        for (i = 0; i < N; i++)
-            for (j = 0; j < N; j++)
-                // All sites are blocked by default
-                id[i][j] = false;
     }
 
     private void validate(int x, int y) {
@@ -47,6 +49,7 @@ public class Percolation {
 
     public void open(int i, int j) {
         validate(i, j);
+
         int x = i-1;
         int y = j-1;
         id[x][y] = true;
@@ -54,41 +57,60 @@ public class Percolation {
 
         if (size == 1) {
             qf.union(current, top);
+            nf.union(current, top);
             qf.union(current, bottom);
             return;
         }
+
         // union the cells above and below
         if (x == 0) {
-            if (isOpen(x+2, y+1))
+            if (isOpen(x+2, y+1)) {
                 qf.union(current, convert(x+1, y));
+                nf.union(current, convert(x+1, y));
+            }
             qf.union(current, top);
+            nf.union(current, top);
         }
         else if (x == size-1) {
-            if (isOpen(x, y+1))
+            if (isOpen(x, y+1)) {
                 qf.union(current, convert(x-1, y));
+                nf.union(current, convert(x-1, y));
+            }
             qf.union(current, bottom);
         }
         else {
-            if (isOpen(x, y+1))
+            if (isOpen(x, y+1)) {
                 qf.union(current, convert(x-1, y));
-            if (isOpen(x+2, y+1))
+                nf.union(current, convert(x-1, y));
+            }
+            if (isOpen(x+2, y+1)) {
+                nf.union(current, convert(x+1, y));
                 qf.union(current, convert(x+1, y));
+            }
         }
 
         // union the cells left and right
         if (y != size-1 && y != 0) {
-            if (isOpen(x+1, y))
+            if (isOpen(x+1, y)) {
+                nf.union(current, convert(x, y-1));
                 qf.union(current, convert(x, y-1));
-            if (isOpen(x+1, y+2))
+            }
+            if (isOpen(x+1, y+2)) {
+                nf.union(current, convert(x, y+1));
                 qf.union(current, convert(x, y+1));
+            }
         }
         else if (y == size - 1) {
-            if (isOpen(x+1, y))
+            if (isOpen(x+1, y)) {
+                nf.union(current, convert(x, y-1));
                 qf.union(current, convert(x, y-1));
+            }
         }
         else if (y == 0) {
-            if (isOpen(x+1, y+2))
+            if (isOpen(x+1, y+2)) {
+                nf.union(current, convert(x, y+1));
                 qf.union(current, convert(x, y+1));
+            }
         }
     }
 
@@ -99,26 +121,10 @@ public class Percolation {
 
     public boolean isFull(int i, int j) {
         validate(i, j);
-        return qf.connected(convert(i-1, j-1), top);
+        return nf.connected(convert(i-1, j-1), top);
     }
 
     public boolean percolates() {
         return qf.connected(top, bottom);
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Enter the size of the grid");
-        int size = StdIn.readInt();
-        Percolation p = new Percolation(size);
-        while (true) {
-            System.out.println("Enter a cell to open");
-            int i = StdIn.readInt();
-            int j = StdIn.readInt();
-            p.open(i, j);
-            if (p.percolates())
-                System.out.println("Percolates");
-            else
-                System.out.println("Does not percolate");
-        }
     }
 }
